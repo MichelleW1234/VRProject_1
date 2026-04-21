105171d31175e4c78ed51d878087f6c86d7cfa06
Michelle Wee <122498397+MichelleW1234@users.noreply.github.com>
2026-04-20 17:50:14 -0700

    Add files via upload

---
+++ Scripts/Player.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using Unity.Engine.InputSystem;
+using Unity.Mathematics;
+
+public class Player : MonoBehaviour
+{
+    public InputActionReference placeAction;
+    public Vector3 playerPosition;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        placeAction.action.performed += PlaceFunction;
+        placeAction.action.Enable();
+
+    }
+
+    private void OnDisable()
+    {
+        placeAction.action.performed -= PlaceFunction;
+        placeAction.action.Disable();
+
+    }
+
+    private void PlaceFunction(InputAction.CallbackContext context)
+    {
+        
+        GameObject newObject1 = GameObject.Find("Object_1");
+        Vector3 objectInitialPosition = transform.forward;
+        Quaternion objectInitialRotation = quaternion.identity;
+        GameObject.Insantiate(newObject1, objectInitialPosition, objectInitialRotation);
+
+    }
+}
